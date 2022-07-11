8807c38f96cf301886d027e332d163c612ed62bb
jootang2 <joohwan.song15b@gmail.com>
2022-07-11 15:01:29 +0900

    삭제기능구현

--- src/main/java/com/ll/exam/App.java
+++ src/main/java/com/ll/exam/App.java
@@ -1,5 +1,7 @@
 package com.ll.exam;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
@@ -9,7 +11,10 @@ public class App {
 
 
         Scanner sc = new Scanner(System.in);
+        List<WiseSaying> wiseSayings = new ArrayList<>();
         int wiseSayingLastId = 0; //마지막 명언글의 번호
+
+
         outer:
 
 
@@ -17,7 +22,9 @@ public class App {
             System.out.print("명령) ");
             String cmd = sc.nextLine().trim();
 
-            switch (cmd) {
+            Rq rq = new Rq (cmd);
+            switch (rq.getPath()){
+
                 case "등록":
 
                     System.out.print("명언 : ");
@@ -25,10 +32,58 @@ public class App {
                     System.out.print("작가 : ");
                     String author = sc.nextLine().trim();
                     int id = ++wiseSayingLastId;
-
                     WiseSaying wiseSaying = new WiseSaying(id, content, author);
+
+                    wiseSayings.add(wiseSaying);
                     System.out.printf("%d번 명언이 등록되었습니다.\n", id);
                     break;
+
+                case "삭제":
+                    // URL에 입력된 id 얻기
+                    int paramId = rq.getIntParam("id", 0);
+
+                    // URL에 입력된 id가 없다면 작업중지
+                    if (paramId == 0) {
+                        System.out.println("id를 입력해주세요.");
+                        continue;
+                    }
+
+                    // URL에 입력된 id에 해당하는 명언객체 찾기
+                    WiseSaying wiseSaying__ = null;
+
+                    for (WiseSaying wiseSaying___ : wiseSayings) {
+                        if (wiseSaying___.id == paramId) {
+                            wiseSaying__ = wiseSaying___;
+                        }
+                    }
+
+                    // 찾지 못했다면 중지
+                    if (wiseSaying__ == null) {
+                        System.out.printf("%d번 명언은 존재하지 않습니다..\n", paramId);
+                        continue;
+                    }
+
+                    // 입력된 id에 해당하는 명언객체를 리스트에서 삭제
+                    wiseSayings.remove(wiseSaying__);
+
+                    System.out.printf("%d번 명언이 삭제되었습니다.\n", paramId);
+                    break;
+
+                case "목록":
+
+                    System.out.println("번호 / 작가 / 명언");
+                    System.out.println("----------------");
+                    for(int i = wiseSayings.size() - 1 ; i >=0; i--) {
+
+                        WiseSaying wiseSaying_ = wiseSayings.get(i);
+                        System.out.printf("%d  /  %s / %s \n" , wiseSaying_.id, wiseSaying_.content, wiseSaying_.author);
+                    }
+
+                    break;
+
+
+
+
                 case "종료":
                     break outer;
 
--- src/test/java/com/ll/exam/Apptest.java
+++ src/test/java/com/ll/exam/Apptest.java
@@ -2,21 +2,49 @@ package com.ll.exam;
 
 import org.junit.jupiter.api.Test;
 
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
+import java.io.*;
 import java.util.Scanner;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class Apptest {
+class AppTest {
     @Test
-    public void 테스트_실험(){
+    public void Rq__getPath() {
+        Rq rq = new Rq("삭제?id=1");
+
+        String path = rq.getPath();
+
+        assertEquals("삭제", path);
+    }
+
+    @Test
+    public void Rq__getIntParam() {
+        Rq rq = new Rq("삭제?id=1");
+
+        int id = rq.getIntParam("id", 0);
+
+        assertEquals(1, id);
+    }
+
+    @Test
+    public void Rq__getIntParam__2() {
+        Rq rq = new Rq("검색?id=10&no=1");
+
+        int id = rq.getIntParam("id", 0);
+        int no = rq.getIntParam("no", 0);
+
+        assertEquals(10, id);
+        assertEquals(1, no);
+    }
+
+    @Test
+    public void 테스트_실험() {
         int rs = 10 + 20;
         assertEquals(30, rs);
     }
 
     @Test
-    public void 테스트_스캐너(){
+    public void 문자열을_스캐너의_입력으로_설정() {
         String input = """
                 등록
                 명언1
@@ -32,6 +60,23 @@ public class Apptest {
         assertEquals("등록", cmd);
         assertEquals("명언1", content);
         assertEquals("작가1", author);
+    }
 
+    @Test
+    public void 표준출력을_리다이렉션하여_결과를_문자열로_받기() throws IOException {
+        // 표준출력을 리다이렉션
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(output));
+
+        System.out.println("안녕");
+
+        // 그 동안 System.out.println 으로 모아놨던 문장들을 받아옴
+        String rs = output.toString().trim();
+
+        // 표준출력을 원상복구
+        System.setOut(new PrintStream(new FileOutputStream(FileDescriptor.out)));
+        output.close();
+
+        assertEquals("안녕", rs);
     }
 }
